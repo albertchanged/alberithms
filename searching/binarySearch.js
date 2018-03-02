6af2dbe60f951cc6c7275624d5c5eea40988ff15
Albert Chang <albertchanged@gmail.com>
2018-03-02 11:06:45 -0800

    Push updated (N --> log N) recursive solution to binarySearch

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -5,7 +5,7 @@
 // 4. Define middle in that side
 // 5. Repeat until we find value
 
-var binarySearchRecursive = (array, target) => {
+var binarySearchRecursiveN = (array, target) => {
   // Initialize middle
   let middle = Math.floor(array.length / 2);
   // If we find target right away, return true
@@ -19,6 +19,33 @@ var binarySearchRecursive = (array, target) => {
     return binarySearchRecursive(array.slice(middle + 1), target);
   }
 };
+
+
+var binarySearchRecursiveHelperLogN = (array, target, left, right) => {
+  // Initialize middle
+  let middle = Math.floor((left + right) / 2);
+  // If left overlaps right, then we have not found target -- return false
+  if (left >= right) {
+    return false;
+  }
+  // If we find target right away, return true
+  if (target === array[middle]) {
+    return true;
+  } else if (target < array[middle]) {
+    // Otherwise search left side if target is less
+    return binarySearchRecursiveHelperLogN(array, target, 0, middle);
+  } else if (target > array[middle]) {
+    // Otherwise search right side if target is more
+    return binarySearchRecursiveHelperLogN(array, target, middle + 1, array.length - 1);
+  }
+};
+
+var binarySearchRecursiveLogN = (array, target) => {
+  return binarySearchRecursiveHelperLogN(array, target, 0, array.length  - 1);
+};
+
+console.log('1 is inside: ', binarySearchRecursiveLogN([1, 2, 3, 4, 5, 6, 7, 9], 1));
+console.log('8 is inside: ', binarySearchRecursiveLogN([1, 2, 3, 4, 5, 6, 7, 9], 8));
 
 var binarySearchIterative = (array, target) => {
   // Initialize left and right
@@ -41,5 +68,5 @@ var binarySearchIterative = (array, target) => {
   return target === array[middle];
 };
 
-console.log(binarySearchRecursive([1, 2, 3, 4, 5, 6, 7], 3));
-console.log(binarySearchIterative([1, 2, 3, 4, 5, 6, 7], 4));
+// console.log(binarySearchRecursive([1, 2, 3, 4, 5, 6, 7], 3));
+// console.log(binarySearchIterative([1, 2, 3, 4, 5, 6, 7], 4));
