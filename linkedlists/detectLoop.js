2cabe515e1a2de926d6ccddc5707338cb65a421e
Albert Chang <albertchanged@gmail.com>
2018-03-04 22:55:15 -0800

    Add test cases for detectLoop.js

--- linkedlists/detectLoop.js
+++ linkedlists/detectLoop.js
@@ -20,3 +20,16 @@ Node.prototype.containsLoop = function(node) {
 }
 
 
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node3;
+
+console.log(node1.containsLoop(node5));
